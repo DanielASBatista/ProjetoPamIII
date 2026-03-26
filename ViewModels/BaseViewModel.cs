e83332cbd75fb5b989c10ed34cf5fe35d3ffa7c7
Rafael Martins Nogueira <rafamartnogueira@gmail.com>
2026-03-25 22:22:03 -0300

    first commit.

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AppRpgEtec.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke
+                (this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
